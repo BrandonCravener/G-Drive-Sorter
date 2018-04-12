9eceea5d57b38a7fc0e9a61d83a901d55cfff177
Fernando <2021arrfera@share.wilsonsd.org>
2018-04-11 20:31:36 -0400

    Add delete account button to settings tab component

--- src/app/components/tabs/settings/settings.component.ts
+++ src/app/components/tabs/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { auth } from 'firebase/app';
+import { FirebaseService } from '../../../services/firebase/firebase.service';
+import { GoogleService } from '../../../services/google/google.service';
 
 /**
  * Declare the component to be shown when the settings tab is selected.
@@ -10,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss', '../../../../simple-grid.scss']
 })
 export class SettingsComponent implements OnInit {
   /**
@@ -25,6 +28,20 @@ export class SettingsComponent implements OnInit {
    * @memberof SettingsComponent
    */
   ngOnInit() {
+    // variables
+    let deleteAccountButton = document.getElementById('delete-account-button');
+    let user = auth().currentUser;
+
+    function deleteAccount() {
+      user.delete().then(function() {
+        // User deleted.
+      }).catch(function(error) {
+        // An error happened.
+      });
+      gapi.auth.signOut();
+    }
+
+    deleteAccountButton.addEventListener("click", deleteAccount);
   }
 
 }
